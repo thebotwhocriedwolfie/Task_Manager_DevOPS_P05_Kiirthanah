c02949b04849d859667aba527f028c6f86ded9b9
napsyy <muhammadakmal080904@gmail.com>
2024-11-10 21:46:30 +0800

    Implemented category filtering

--- public/js/view_tasks.js
+++ public/js/view_tasks.js
@@ -1,39 +1,56 @@
-function loadTasks() {
+function loadCategoryDropdown() {
+  var request = new XMLHttpRequest();
+  request.open('GET', '/view-categories', true);
+  request.setRequestHeader('Content-Type', 'application/json');
+
+  request.onload = function () {
+      var categories = JSON.parse(request.responseText);
+      var optionsHtml = '';
+
+      // Generate <option> elements for each category
+      categories.forEach(category => {
+          optionsHtml += `<option value="${category.name}">${category.name}</option>`;
+      });
+
+      // Insert the generated options into the categoryDropdown select element
+      document.getElementById('categoryDropdown').innerHTML += optionsHtml;
+  };
+
+  request.send();
+}
+
+// Call loadCategoryDropdown when the page loads to display category options
+document.addEventListener('DOMContentLoaded', loadCategoryDropdown);
+
+function loadTasks(category = "") {
     var response = "";
     var request = new XMLHttpRequest();
     request.open("GET", "/view-tasks", true);
     request.setRequestHeader("Content-Type", "application/json");
     request.onload = function () {
       response = JSON.parse(request.responseText);
+      filteredTasks = response;
+
+      // Filter tasks by category if a category is provided
+      if (category) {
+        filteredTasks = response.filter(task => task.category === category);
+      }
+
       var html = "";
-      for (var i = 0; i < response.length; i++) {
+      for (var i = 0; i < filteredTasks.length; i++) {
         html +=
           "<tr>" +
-          "<td>" +
-          (i + 1) +
-          "</td>" +
-          "<td>" +
-          response[i].name +
-          "</td>" +
-          "<td>" +
-          response[i].category +
-          "</td>" +
-          "<td>" +
-          response[i].description +
-          "</td>" +
-          "<td>" +
-          response[i].start_time +
-          "</td>" +
-          "<td>" +
-          response[i].end_time +
-          "</td>" +
+          "<td>" + (i + 1) + "</td>" +
+          "<td>" + filteredTasks[i].name + "</td>" +
+          "<td>" + filteredTasks[i].category + "</td>" +
+          "<td>" + filteredTasks[i].description + "</td>" +
+          "<td>" + filteredTasks[i].start_time + "</td>" +
+          "<td>" + filteredTasks[i].end_time + "</td>" +
           "<td>" +
           '<button type="button" class="btn btn-warning" onclick="editTask(\'' +
-          JSON.stringify(response[i]).replaceAll('"', "&quot;") +
-          "')\">Edit</button> " +
+          JSON.stringify(response[i]).replaceAll('"', "&quot;") + "')\">Edit</button> " +
           '<button type="button" class="btn btn-danger" onclick="deleteTask(' +
-          response[i].id +
-          ')"> Delete</button>' +
+          response[i].id + ')"> Delete</button>' +
           "</td>" +
           "</tr>";
       }
